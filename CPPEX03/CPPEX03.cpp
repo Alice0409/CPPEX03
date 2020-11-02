8ff1ddc34371697835a3bc90c0c9f45861d7262e
Alice <63988826+Alice0409@users.noreply.github.com>
2020-11-02 14:48:44 +0900

    Add files via upload

---
+++ CPPEX03/CPPEX03.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+#include "getBmi.cpp"
+#include "getObesity.cpp"
+
+int main() {
+	double height;
+	double weight;
+	double bmi;
+	int num;
+
+	cout << "身長（cm）を入力してください：";
+	cin >> height;
+
+	cout << "体重（kg）を入力してください：";
+	cin >> weight;
+
+	bmi = getBmi(height, weight);
+	cout << "あなたのBMIは、" << bmi << "です。" << endl;
+
+	num = getObesity(bmi);
+	switch (num) {
+	case -1:
+		cout << "低体重です。";
+		break;
+	case 0:
+		cout << "普通体重です。";
+		break;
+	case 1:
+		cout << "肥満(1度)です。";
+		break;
+	case 2:
+		cout << "肥満(2度)です。";
+		break;
+	case 3:
+		cout << "肥満(3度)です。";
+		break;
+	case 4:
+		cout << "肥満(4度)です。";
+		break;
+	}
+	int e;
+	cin >> e;
+	return 0;
+}
---
+++ CPPEX03/getObesity.cpp
@@ -0,0 +1,11 @@
+inline int getObesity(double bmi) {
+	int num;
+
+	if (bmi < 18.5) num = -1;
+	else if (bmi < 25) num = 0;
+	else if (bmi < 30) num = 1;
+	else if (bmi < 35) num = 2;
+	else if (bmi < 40) num = 3;
+	else num = 4;
+	return num;
+}
